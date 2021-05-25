adaa279acc1cc57c3f96f6f177480d462d39acac
Victor Silva <victormmsilva97@gmail.com>
2021-05-25 16:52:03 -0300

    install sequelize

--- packages/back/app.js
+++ packages/back/app.js
@@ -1,30 +1,27 @@
-const mysql = require('mysql2');
+const { Sequelize } = require('sequelize');
 const express = require('express');
 const app = express();
 const port = 3001;
 
-const connection = mysql.createConnection({
-  host: 'localhost', // O host do banco. Ex: localhost
+const sequelize = new Sequelize(
+  'teste_native',
+  'root',
+  '', {
+  host: 'localhost',
   port: '3306',
-  user: 'root', // Um usuário do banco. Ex: user 
-  database: 'teste_native', // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
+  dialect: 'mysql',/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log('Erro connecting to database...', err.message)
-    return
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
   }
-  console.log('Connection established!')
-})
+};
 
-connection.end((err) => {
-  if (err) {
-    console.log('Erro to finish connection...', err.message)
-    return
-  }
-  console.log('The connection was finish...')
-})
+testConnection();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
