6801affd472284582d03f269026c275ba769fa12
Victor Silva <victormmsilva97@gmail.com>
2021-05-25 16:30:02 -0300

    mysql connection

--- packages/back/app.js
+++ packages/back/app.js
@@ -1,6 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const connection = mysql.createConnection({
+  host: 'localhost', // O host do banco. Ex: localhost
+  port: '3306',
+  user: 'root', // Um usuário do banco. Ex: user 
+  database: 'teste_native', // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('Erro connecting to database...', err.message)
+    return
+  }
+  console.log('Connection established!')
+})
+
+connection.end((err) => {
+  if (err) {
+    console.log('Erro to finish connection...', err.message)
+    return
+  }
+  console.log('The connection was finish...')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
